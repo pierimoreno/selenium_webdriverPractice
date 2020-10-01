4ad50cfb1a2cc328353d87c2850e052d00fcf69a
Jayesh Gupta <60746083+jayeshgupta91@users.noreply.github.com>
2020-10-01 18:15:30 +0530

    drive.implicitly_wait() used for save time
    drive.implicitly_wait() used for save time and page will load fully.

--- user_registration.py
+++ user_registration.py
@@ -5,14 +5,14 @@ import time
 
 drive = webdriver.Chrome(r"C:\Users\pieri\PycharmProjects\appPython\Selenium\chromedriver.exe")
 drive.get("https://pluto.tv/live-tv/threes-company")
-time.sleep(8)
+drive.implicitly_wait(30)
 
 #click on emerges windows
 drive.find_element_by_css_selector('[class="MuiButtonBase-root MuiButton-root jss9 MuiButton-text jss10 MuiButton-textSizeLarge MuiButton-sizeLarge"]>[class="MuiButton-label jss8"]').click()
 drive.find_element_by_css_selector('[class="MuiSvgIcon-root jss71 jss72"]').click()
 
 drive.find_element_by_class_name("jss23.jss24").click()
-time.sleep(2)
+drive.implicitly_wait(10)
 drive.find_element_by_class_name("MuiButton-label").click()
 
 
@@ -22,12 +22,12 @@ drive.find_element_by_css_selector('[name="firstName"]').send_keys("Pierina")
 
 #insert email
 gmail = drive.find_element_by_name("email").click()
-time.sleep(1)
+drive.implicitly_wait(10)
 drive.find_element_by_name("email").send_keys("pierinamoreno@gmail.com")
 
 #insert password
 password = drive.find_element_by_name("password").click()
-time.sleep(1)
+drive.implicitly_wait(10)
 drive.find_element_by_name("password").send_keys("television18.")
 
 #year selection
